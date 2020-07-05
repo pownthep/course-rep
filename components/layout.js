6d547a82e1b3c4d794c8d80156d92c158ab9a08d
pownthep <pownthepofficial@gmail.com>
2020-07-05 18:54:18 +0700

    import nav

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "./nav";
+import Nav from "./Nav";
 import { FirebaseContext } from "../components/Firebase";
 
 export default function Layout({ children }) {
